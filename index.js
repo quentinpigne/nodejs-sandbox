42c63067309e886818a17b86439cb0edffa38035
Quentin Pigné <quentin.pigne@gmail.com>
2021-01-22 00:54:37 +0100

    add body-parser library and post endpoint

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict';
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+})
+
+app.post('/', (req, res) => {
+  res.send(req.body);
 })
 
 app.listen(port, () => {
