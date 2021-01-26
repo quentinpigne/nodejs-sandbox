bd8099ea2b2e93ec44b520377c56fa7c5910347d
Quentin Pigné <quentin.pigne@gmail.com>
2021-01-26 20:00:54 +0100

    automatic routes discovery using route folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,17 @@
 'use strict';
 const { Router } = require("express");
+const changeCase = require("change-case");
+
+const routes = require("require-dir")();
 
 module.exports = () => {
   const router = Router();
 
-  router.use('/test', require("./test")());
+  // Initialize all routes
+  Object.keys(routes).forEach(routeName => {
+    // Add route to the speficied route name
+    router.use('/' + changeCase.paramCase(routeName), routes[routeName]());
+  });
 
   return router;
 }
