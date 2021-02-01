667d9656dd0fcb6035505bb538b544ab4c01abfa
Quentin Pigné <quentin.pigne@gmail.com>
2021-02-01 19:25:49 +0100

    errors handling

--- config/initializers/server.js
+++ config/initializers/server.js
@@ -19,6 +19,35 @@ module.exports = function(callback) {
 
   app.use(nconf.get("api:prefix"), require("../../app/routes")());
 
+  // Catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+  });
+
+  // Error handlers
+  app.use((err, req, res, next) => {
+    /**
+     * Handle 401 thrown by express-jwt library
+     */
+    if (err.name === 'UnauthorizedError') {
+      return res
+        .status(err.status)
+        .send({ message: err.message })
+        .end();
+    }
+    return next(err);
+  });
+
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: (app.get('env') === 'development' ? err : {})
+    });
+  });
+
   app.listen(port, () => {
     logger.info(`[SERVER] Listening on port ${port}`);
     callback();
